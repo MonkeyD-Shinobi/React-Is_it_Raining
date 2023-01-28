7b4b9ff592c1fed0ff0425e33a9853944e610aef
MonkeyD-Shinobi <108976808+MonkeyD-Shinobi@users.noreply.github.com>
2023-01-28 11:55:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// Given a class component, add state to it
+// state should have a property called `isRaining` which is a boolean
+// (true if it is raining, false if not)
+// Render the word "Yes" if it is raining
+// or "No" if it is not raining
+
+// QUESTION :-- React - Is it Raining?
+// Create a boolean state named 'itsRaining' in a classComponent.
+// Initially, set it as false. Display “Is it Raining today? Yes”,
+// if itsRaining is true else “Is it Raining today? No”
+
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isRaining: false
+    };
+  }
+  render() {
+    return <p>Is it Raining today? {this.state.isRaining ? "Yes" : "No"}</p>;
+  }
+}
+
+export default App;
